b405bf61aeb7b24ece774dd829bebd390d206508
Kevin Burk <percburk@gmail.com>
2021-02-09 15:50:40 -0600

    initial functionality of dashboard completed

--- server/routes/coffee.router.js
+++ server/routes/coffee.router.js
@@ -5,12 +5,13 @@ const router = express.Router();
 router.get('/', (req, res) => {
   const sqlText = `
     SELECT "coffees".id, "coffees".date, "coffees".roaster, "coffees".country,
-    "coffees".producer, "coffees".blend_name, "users_coffees".is_fav, 
+    "coffees".producer, "coffees".blend_name, "coffees".coffee_pic, "users_coffees".is_fav,
     ARRAY_AGG("coffees_flavors".flavors_id) AS "flavors_array" 
-    FROM "coffees_flavors" 
+    FROM "coffees_flavors"
     JOIN "coffees" ON "coffees_flavors".coffees_id = "coffees".id
     JOIN "users_coffees" ON "coffees".id = "users_coffees".coffees_id
-    WHERE "users_coffees".users_id = $1 GROUP BY "coffees".id 
+    WHERE "users_coffees".users_id = $1 
+    GROUP BY "coffees".id, "users_coffees".is_fav
     ORDER BY "coffees".date;
   `;
 
@@ -19,6 +20,21 @@ router.get('/', (req, res) => {
     .then((response) => res.send(response.rows))
     .catch((err) => {
       console.log(`error in GET with query ${sqlText}`, err);
+      res.sendStatus(500);
+    });
+});
+
+router.put('/fav/:id', (req, res) => {
+  const sqlText = `
+    UPDATE "users_coffees" SET "is_fav" = NOT "is_fav" 
+    WHERE "users_id" = $1 AND "coffees_id" = $2;
+  `;
+
+  pool
+    .query(sqlText, [req.user.id, req.params.id])
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+      console.log(`error in PUT with query ${sqlText}`, err);
       res.sendStatus(500);
     });
 });
